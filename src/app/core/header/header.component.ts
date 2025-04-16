a6db47e67d0492aad6b9f867659a17f2f620a2c5
Agnieszka Pawlak <agnieszka@pawlak.io>
2025-04-16 23:39:34 +0200

    Add navigation

---
+++ src/app/core/header/header.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterLink} from '@angular/router';
+import {TranslateModule, TranslatePipe, TranslateService} from '@ngx-translate/core';
+import {FormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-header',
+  imports: [TranslateModule, CommonModule, RouterLink, TranslatePipe, FormsModule],
+  templateUrl: './header.component.html',
+  standalone: true,
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  currentLanguage = 'en';
+  constructor(private translate: TranslateService) {
+    this.currentLanguage = this.translate.currentLang || 'en';
+     this.translate.addLangs(['pl', 'en']);
+     this.translate.setDefaultLang('pl');
+  }
+
+  navLinks = [
+    {label: "nav.label.articles", path: './articles'},
+    {label: "nav.label.projects", path: './projects'},
+    {label: "nav.label.contact", path: './contact'}
+  ]
+
+  switchLanguage(language: string): void {
+    // this.translate.setDefaultLang(language);
+    this.currentLanguage = language;
+    this.translate.use(language);
+
+  }
+
+}
