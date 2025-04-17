a9686f02f850a30abc5b6668e3e45c8c7e1e406c
Agnieszka Pawlak <agnieszka@pawlak.io>
2025-04-17 14:11:43 +0200

    add home page router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
   {
+    path: 'home',
+    loadComponent: () => import('./home/home.component').then(m => m.HomeComponent),
+  },
+  {
     path: 'contact',
     loadComponent: () => import('./contact/contact.component').then(m => m.ContactComponent),
   },
