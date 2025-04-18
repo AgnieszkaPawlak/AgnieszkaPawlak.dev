f720bf490ed317158563eef4bb34e07d80e30c3c
Agnieszka Pawlak <agnieszka@pawlak.io>
2025-04-18 11:18:07 +0200

    use typing effect component on the home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { TypingEffectComponent } from '../shared/typing-effect/typing-effect.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
+  imports: [
+    TypingEffectComponent
+  ],
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  protected texts = signal<string[]>([
+    '    My experience includes delivering high-quality frontend libraries and ensuring applications run flawlessly through meticulous testing.\n' +
+    '      Currently, I am expanding my expertise to become a full-stack developer by learning Java and backend object-oriented design patterns.'
+  ]);
+  protected typingDelay = signal<number>(2500);
 
 }
