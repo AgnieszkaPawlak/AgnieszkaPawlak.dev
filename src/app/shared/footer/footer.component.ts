788b40148ceac8f4b24a4b7fe633143393609d9e
Agnieszka Pawlak <agnieszka@pawlak.io>
2025-04-23 13:00:08 +0200

    add translation for footer component

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {TranslatePipe} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [CommonModule],
+  imports: [CommonModule, TranslatePipe],
   templateUrl: './footer.component.html',
   standalone: true,
   styleUrl: './footer.component.scss'
