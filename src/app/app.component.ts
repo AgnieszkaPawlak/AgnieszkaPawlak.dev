238a0d07039fd559aecbf4f02a2a1f9938f8ef41
Agnieszka Pawlak <agnieszka@pawlak.io>
2025-04-29 14:01:06 +0200

    add missing padding for the main section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {FooterComponent} from './shared/footer/footer.component';
 import {HeaderComponent} from './shared/header/header.component';
 import {RouterOutlet} from '@angular/router';
+import {LayoutService} from '@app/shared/services/layout.service';
 
 @Component({
   selector: 'app-root',
@@ -11,5 +12,8 @@ import {RouterOutlet} from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  constructor(public layoutService: LayoutService) {
+  }
+
   title = 'my-blog';
 }
