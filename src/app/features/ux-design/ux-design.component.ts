db2e7bb55b412e297b2fa503e0177a7a2fe9f7bf
AgnieszkaPawlak <agnieszka@pawlak.io>
2025-07-28 15:41:19 +0200

    feature: add ux designs page - linting issues

--- src/app/features/ux-design/ux-design.component.ts
+++ src/app/features/ux-design/ux-design.component.ts
@@ -1,33 +1,30 @@
-import { Component } from '@angular/core';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import {Component} from '@angular/core';
+import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 
 @Component({
-  selector: 'app-ux-design',
-  imports: [],
-  templateUrl: './ux-design.component.html',
-  styleUrl: './ux-design.component.scss',
-  standalone: true
+    selector: 'app-ux-design',
+    imports: [],
+    templateUrl: './ux-design.component.html',
+    styleUrl: './ux-design.component.scss',
+    standalone: true
 })
 export class UxDesignComponent {
 
 
-  projects: { title: string; url: SafeResourceUrl }[];
+    projects: { title: string; url: SafeResourceUrl }[];
 
-  constructor(private sanitizer: DomSanitizer) {
-    this.projects = [
-      {
-        title: 'AiAgent',
-        url: this.sanitizeUrl(
-          'https://www.figma.com/embed?embed_host=share&url=https://www.figma.com/design/WK9VEdOC2BQGNzcyRmi6nv/AiAgent?m=auto&t=tRcYIeeLv6AFZgvc-1'
-        ),
-      },
+    constructor(private sanitizer: DomSanitizer) {
+        this.projects = [
+            {
+                title: 'AiAgent',
+                url: this.sanitizeUrl(
+                    'https://www.figma.com/embed?embed_host=share&url=https://www.figma.com/design/WK9VEdOC2BQGNzcyRmi6nv/AiAgent?m=auto&t=tRcYIeeLv6AFZgvc-1'
+                ),
+            },
+        ];
+    }
 
-    ];
-  }
-
-  private sanitizeUrl(url: string): SafeResourceUrl {
-    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
-  }
-
-
+    private sanitizeUrl(url: string): SafeResourceUrl {
+        return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+    }
 }
